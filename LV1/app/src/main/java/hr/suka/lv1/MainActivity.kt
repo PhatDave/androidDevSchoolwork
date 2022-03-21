70b838218cad4e51b505fe06a17a6641521b71d3
PhatPhuckDave <kosmodiskclassic0@gmail.com>
2022-03-21 18:59:17 +0100

    Implement luka modric best footballer

--- LV1/app/src/main/java/hr/suka/lv1/MainActivity.kt
+++ LV1/app/src/main/java/hr/suka/lv1/MainActivity.kt
@@ -2,10 +2,31 @@ package hr.suka.lv1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity: ", "onCreate")
+
+        val textView = findViewById(R.id.textView1) as TextView
+
+        textView.setOnClickListener {
+            Log.d("MainActivity: ", "POG")
+            textView.setText("AAAAAAAAAAA")
+        }
+    }
+    
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity: ", "onPause")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity: ", "onDestroy")
     }
 }
