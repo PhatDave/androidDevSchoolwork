dffbb1e6923e800f3683c114d9f339bd5ef19744
PhatPhuckDave <kosmodiskclassic0@gmail.com>
2022-03-21 19:21:03 +0100

    Implement bmi calculation functionality

--- LV1/app/src/main/java/hr/suka/lv1/MainActivity.kt
+++ LV1/app/src/main/java/hr/suka/lv1/MainActivity.kt
@@ -1,9 +1,18 @@
 package hr.suka.lv1
 
+import android.R.attr
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+
+import android.R.attr.data
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,21 +21,23 @@ class MainActivity : AppCompatActivity() {
 
         Log.d("MainActivity: ", "onCreate")
 
-        val textView = findViewById(R.id.textView1) as TextView
+        val entry1 = findViewById<EditText>(R.id.entry1)
+        val entry2 = findViewById<EditText>(R.id.entry2)
+        val bmiSubmit = findViewById<Button>(R.id.bmiSubmit)
 
-        textView.setOnClickListener {
-            Log.d("MainActivity: ", "POG")
-            textView.setText("AAAAAAAAAAA")
+        bmiSubmit.setOnClickListener {
+            val height = entry1.text.toString()
+            val weight = entry2.text.toString()
+
+            val intHeight = height.toFloat()
+            val intWeight = weight.toFloat()
+
+            val bmi = calculateBmi(intHeight, intWeight)
+            Toast.makeText(this, bmi.toString(), Toast.LENGTH_LONG).show()
         }
     }
-    
-    override fun onPause() {
-        super.onPause()
-        Log.d("MainActivity: ", "onPause")
-    }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("MainActivity: ", "onDestroy")
+    fun calculateBmi(height: Float, weight: Float): Float {
+        return weight / (height * height)
     }
 }
