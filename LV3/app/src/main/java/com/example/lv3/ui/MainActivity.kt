888e9b89a7f8a94ff1f1c0cc1f4576c1cccdd2cb
PhatDave <kosmodiskclassic0@gmail.com>
2022-04-06 18:13:09 +0200

    Add some stuff and things like recyclerview and packages

--- LV3/app/src/main/java/com/example/lv3/ui/MainActivity.kt
+++ LV3/app/src/main/java/com/example/lv3/ui/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.example.lv3
+package com.example.lv3.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lv3.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var recyclerView = findViewById<RecyclerView>(R.id.recyclerView);
+        
     }
 }
