2342e04c203b14e17ce77e11e9e9f00bb19426c2
PhatDave <kosmodiskclassic0@gmail.com>
2022-04-06 18:20:37 +0200

    Add dataSource

---
+++ LV3/app/src/main/java/com/example/lv3/data/DataSource.kt
@@ -0,0 +1,14 @@
+package com.example.lv3.data
+
+import com.example.lv3.R
+import com.example.lv3.model.Task
+
+class DataSource {
+    fun loadTasks() : List<Task> {
+        return listOf<Task>(
+            Task(R.string.task1),
+            Task(R.string.task2),
+            Task(R.string.task3)
+        )
+    }
+}
