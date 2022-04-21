0a3ddb8bf44ac3057051205cb18bef9268fd21d2
PhatDave <kosmodiskclassic0@gmail.com>
2022-04-21 18:28:52 +0200

    Fully implement accelerometer and UI updates

--- LV4/app/src/main/java/com/example/lv4/MainActivity.kt
+++ LV4/app/src/main/java/com/example/lv4/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.lv4
 
+import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -7,13 +8,15 @@ import android.hardware.SensorManager
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
+	private val decimalFormat = DecimalFormat("0.00")
 	private lateinit var sensorManager: SensorManager
 	private lateinit var accelerationSensor: Sensor
 
-	private val accelerationDisplay = findViewById<TextView>(R.id.accelerationDisplay)
-	private val accelerationComponents = findViewById<TextView>(R.id.accelerationComponents)
+	private lateinit var accelerationDisplay: TextView
+	private lateinit var accelerationComponents: TextView
 
 	private val sensorListener = object : SensorEventListener {
 		override fun onSensorChanged(event: SensorEvent?) {
@@ -21,9 +24,7 @@ class MainActivity : AppCompatActivity() {
 			updateUi(values)
 		}
 
-		override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-			TODO("Not yet implemented")
-		}
+		override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
 	}
 
 	fun updateUi(sensorValues: FloatArray) {
@@ -32,12 +33,29 @@ class MainActivity : AppCompatActivity() {
 			sum += value.toDouble() * value.toDouble()
 		}
 		val totalAcceleration = Math.sqrt(sum)
-		accelerationDisplay.text = "Total acceleration: $totalAcceleration"
-		accelerationComponents.text = "Acceleration: ${sensorValues[0]}, ${sensorValues[1]}, ${sensorValues[2]}"
+		accelerationDisplay.text = "Total acceleration: ${decimalFormat.format(totalAcceleration)}"
+		accelerationComponents.text =
+			"Acceleration\n${sensorValues[0]}\n${sensorValues[1]}\n${sensorValues[2]}"
 	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		accelerationDisplay = findViewById(R.id.accelerationDisplay)
+		accelerationComponents = findViewById(R.id.accelerationComponents)
+
+		sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+		accelerationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+	}
+
+	override fun onPause() {
+		super.onPause()
+		sensorManager.unregisterListener(sensorListener)
+	}
+
+	override fun onResume() {
+		super.onResume()
+		sensorManager.registerListener(sensorListener, accelerationSensor, SensorManager.SENSOR_DELAY_FASTEST)
 	}
 }
