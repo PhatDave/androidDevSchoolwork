28f2eb5e8bb7b57edac534fe4899fef4d344a8e4
PhatDave <kosmodiskclassic0@gmail.com>
2022-04-21 18:00:59 +0200

    Stash

--- LV3/app/src/main/java/com/example/lv3/ui/TaskAdapter.kt
+++ LV3/app/src/main/java/com/example/lv3/ui/TaskAdapter.kt
@@ -10,23 +10,24 @@ import com.example.lv3.R
 import com.example.lv3.model.Task
 
 class TaskAdapter(private val dataSource: List<Task>,
-                  private val context: Context):
-    RecyclerView.Adapter<TaskAdapter.ItemViewHolder>() {
-    class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
-        val textView: TextView = view.findViewById(R.id.textView)
-    }
+                  private val context: Context) :
+	RecyclerView.Adapter<TaskAdapter.ItemViewHolder>() {
+	class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+		val textView: TextView = view.findViewById(R.id.textView)
+	}
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskAdapter.ItemViewHolder {
-        val layoutItem = LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
-        return ItemViewHolder(layoutItem)
-    }
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskAdapter.ItemViewHolder {
+		val layoutItem =
+			LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
+		return ItemViewHolder(layoutItem)
+	}
 
-    override fun onBindViewHolder(holder: TaskAdapter.ItemViewHolder, position: Int) {
-        val task = dataSource[position]
-        holder.textView.text = context.resources.getString(task.stringResId)
-    }
+	override fun onBindViewHolder(holder: TaskAdapter.ItemViewHolder, position: Int) {
+		val task = dataSource[position]
+		holder.textView.text = context.resources.getString(task.stringResId)
+	}
 
-    override fun getItemCount(): Int {
-        return dataSource.size
-    }
+	override fun getItemCount(): Int {
+		return dataSource.size
+	}
 }
