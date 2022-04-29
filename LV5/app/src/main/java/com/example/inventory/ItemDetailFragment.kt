c7e4134d7fcbf92d60518f661b710bce0ec87641
PhatDave <kosmodiskclassic0@gmail.com>
2022-04-29 18:39:21 +0200

    Implement detailed item view

--- LV5/app/src/main/java/com/example/inventory/ItemDetailFragment.kt
+++ LV5/app/src/main/java/com/example/inventory/ItemDetailFragment.kt
@@ -22,56 +22,80 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.inventory.databinding.FragmentItemDetailBinding
+import com.example.inventory.entity.Item
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 /**
  * [ItemDetailFragment] displays the details of the selected item.
  */
 class ItemDetailFragment : Fragment() {
-    private val navigationArgs: ItemDetailFragmentArgs by navArgs()
+	private val navigationArgs: ItemDetailFragmentArgs by navArgs()
 
-    private var _binding: FragmentItemDetailBinding? = null
-    private val binding get() = _binding!!
+	private var _binding: FragmentItemDetailBinding? = null
+	private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentItemDetailBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		_binding = FragmentItemDetailBinding.inflate(inflater, container, false)
+		return binding.root
+	}
 
-    /**
-     * Displays an alert dialog to get the user's confirmation before deleting the item.
-     */
-    private fun showConfirmationDialog() {
-        MaterialAlertDialogBuilder(requireContext())
-            .setTitle(getString(android.R.string.dialog_alert_title))
-            .setMessage(getString(R.string.delete_question))
-            .setCancelable(false)
-            .setNegativeButton(getString(R.string.no)) { _, _ -> }
-            .setPositiveButton(getString(R.string.yes)) { _, _ ->
-                deleteItem()
-            }
-            .show()
-    }
+	/**
+	 * Displays an alert dialog to get the user's confirmation before deleting the item.
+	 */
+	private fun showConfirmationDialog() {
+		MaterialAlertDialogBuilder(requireContext())
+			.setTitle(getString(android.R.string.dialog_alert_title))
+			.setMessage(getString(R.string.delete_question))
+			.setCancelable(false)
+			.setNegativeButton(getString(R.string.no)) { _, _ -> }
+			.setPositiveButton(getString(R.string.yes)) { _, _ ->
+				deleteItem()
+			}
+			.show()
+	}
 
-    /**
-     * Deletes the current item and navigates to the list fragment.
-     */
-    private fun deleteItem() {
-        findNavController().navigateUp()
-    }
+	/**
+	 * Deletes the current item and navigates to the list fragment.
+	 */
+	private fun deleteItem() {
+		findNavController().navigateUp()
+	}
 
-    /**
-     * Called when fragment is destroyed.
-     */
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+	/**
+	 * Called when fragment is destroyed.
+	 */
+	override fun onDestroyView() {
+		super.onDestroyView()
+		_binding = null
+	}
+
+	private val viewModel: InventoryViewModel by activityViewModels {
+		InventoryViewModelFactory(
+			(activity?.application as InventoryApplication).itemRepository.itemDao()
+		)
+	}
+
+	private fun bind(item: Item) {
+		binding.apply {
+			binding.itemName.text = item.name
+			binding.itemPrice.text = item.formattedPrice
+			binding.itemCount.text = item.quantity.toString()
+		}
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		val id = navigationArgs.itemId
+		viewModel.retrieveItem(id).observe(this.viewLifecycleOwner) { selectedItem ->
+			bind(selectedItem)
+		}
+	}
 }
