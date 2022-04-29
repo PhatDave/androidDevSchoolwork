3a5ea0c4c0765dd9b182e91669b1c0e5369a033a
PhatDave <kosmodiskclassic0@gmail.com>
2022-04-29 18:15:19 +0200

    Add view model

--- LV5/app/src/main/java/com/example/inventory/AddItemFragment.kt
+++ LV5/app/src/main/java/com/example/inventory/AddItemFragment.kt
@@ -22,40 +22,77 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.inventory.databinding.FragmentAddItemBinding
+import com.example.inventory.entity.Item
 
 /**
  * Fragment to add or update an item in the Inventory database.
  */
 class AddItemFragment : Fragment() {
 
-    private val navigationArgs: ItemDetailFragmentArgs by navArgs()
+	private val navigationArgs: ItemDetailFragmentArgs by navArgs()
+	private val viewModel: InventoryViewModel by activityViewModels {
+		InventoryViewModelFactory(
+			(activity?.application as InventoryApplication).itemRepository.itemDao()
+		)
+	}
+	lateinit var item: Item
 
-    // Binding object instance corresponding to the fragment_add_item.xml layout
-    // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
-    // when the view hierarchy is attached to the fragment
-    private var _binding: FragmentAddItemBinding? = null
-    private val binding get() = _binding!!
+	// Binding object instance corresponding to the fragment_add_item.xml layout
+	// This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
+	// when the view hierarchy is attached to the fragment
+	private var _binding: FragmentAddItemBinding? = null
+	private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentAddItemBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		_binding = FragmentAddItemBinding.inflate(inflater, container, false)
+		return binding.root
+	}
 
-    /**
-     * Called before fragment is destroyed.
-     */
-    override fun onDestroyView() {
-        super.onDestroyView()
-        // Hide keyboard.
-        val inputMethodManager = requireActivity().getSystemService(INPUT_METHOD_SERVICE) as
-                InputMethodManager
-        inputMethodManager.hideSoftInputFromWindow(requireActivity().currentFocus?.windowToken, 0)
-        _binding = null
-    }
+	private fun isEntryValid(): Boolean {
+		return viewModel.isEntryValid(
+			binding.itemName.text.toString(),
+			binding.itemPrice.text.toString(),
+			binding.itemCount.text.toString()
+		)
+	}
+
+	private fun addNewItem() {
+		if (isEntryValid()) {
+			viewModel.addNewItem(
+				binding.itemName.text.toString(),
+				binding.itemPrice.text.toString(),
+				binding.itemCount.text.toString(),
+			)
+
+			val action = AddItemFragmentDirections.actionAddItemFragmentToItemListFragment()
+			findNavController().navigate(action)
+		}
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		binding.saveAction.setOnClickListener {
+			addNewItem()
+		}
+	}
+
+	/**
+	 * Called before fragment is destroyed.
+	 */
+	override fun onDestroyView() {
+		super.onDestroyView()
+		// Hide keyboard.
+		val inputMethodManager = requireActivity().getSystemService(INPUT_METHOD_SERVICE) as
+				InputMethodManager
+		inputMethodManager.hideSoftInputFromWindow(requireActivity().currentFocus?.windowToken, 0)
+		_binding = null
+	}
 }
